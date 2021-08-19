84e36a06b41658611d634dafe4f4dbd71ca5160f
Andreas Paul <xorpaul@gmail.com>
2021-08-19 16:00:01 +0200

    add vendor and modules

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/xorpaul/check_puppetserver
+
+go 1.16
+
+require (
+	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
+	github.com/xorpaul/go-nagios v0.0.0-20150828095130-23a06a37a851
+)
---
+++ vendor/github.com/xorpaul/go-nagios/nagios.go
@@ -0,0 +1,105 @@
+package nagios
+
+import (
+	"fmt"
+	"github.com/kballard/go-shellquote"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"syscall"
+	"time"
+)
+
+var (
+	Debug     bool
+	buildtime string
+)
+
+type NagiosResult struct {
+	ExitCode  int
+	Text      string
+	Perfdata  string
+	Multiline []string
+}
+
+type ExecResult struct {
+	ReturnCode int
+	Output     string
+}
+
+// Debugf is a helper function for debug logging if Debug is true
+func Debugf(s string) {
+	if Debug != false {
+		fmt.Println("DEBUG " + fmt.Sprint(s))
+	}
+}
+
+// NagiosExit uses the NagiosResult struct to output Nagios plugin compatible output and exit codes
+func NagiosExit(nr NagiosResult) {
+	text := nr.Text
+	exitCode := nr.ExitCode
+	switch {
+	case nr.ExitCode == 0:
+		text = "OK: " + nr.Text
+		exitCode = nr.ExitCode
+	case nr.ExitCode == 1:
+		text = "WARNING: " + nr.Text
+		exitCode = nr.ExitCode
+	case nr.ExitCode == 2:
+		text = "CRITICAL: " + nr.Text
+		exitCode = nr.ExitCode
+	case nr.ExitCode == 3:
+		text = "UNKNOWN: " + nr.Text
+		exitCode = nr.ExitCode
+	default:
+		text = "UNKNOWN: Exit code '" + string(nr.ExitCode) + "'undefined :" + nr.Text
+		exitCode = 3
+	}
+
+	if len(nr.Multiline) > 0 {
+		multiline := ""
+		for _, l := range nr.Multiline {
+			multiline = multiline + l + "\n"
+		}
+		fmt.Printf("%s|%s\n%s\n", text, nr.Perfdata, multiline)
+	} else {
+		fmt.Printf("%s|%s\n", text, nr.Perfdata)
+	}
+	os.Exit(exitCode)
+}
+
+// ExecuteCommand executes a shell command and provides the stdout and stderr output combined and the exit code.
+// First arg is the command to execute as a simple string.
+// Second arg is a timeout parameter as a int after which to kill the command and return.
+// Third arg is a bool flag if the command is allowed to fail or not.
+func ExecuteCommand(command string, timeout int, allowFail bool) ExecResult {
+	Debugf("Executing " + command)
+	parts := strings.SplitN(command, " ", 2)
+	cmd := parts[0]
+	cmdArgs := []string{}
+	if len(parts) > 1 {
+		args, err := shellquote.Split(parts[1])
+		if err != nil {
+			Debugf("executeCommand(): err: " + fmt.Sprint(err))
+			os.Exit(1)
+		} else {
+			cmdArgs = args
+		}
+	}
+
+	before := time.Now()
+	out, err := exec.Command(cmd, cmdArgs...).CombinedOutput()
+	duration := time.Since(before).Seconds()
+	er := ExecResult{0, string(out)}
+	if msg, ok := err.(*exec.ExitError); ok { // there is error code
+		er.ReturnCode = msg.Sys().(syscall.WaitStatus).ExitStatus()
+	}
+	Debugf("Executing " + command + " took " + strconv.FormatFloat(duration, 'f', 5, 64) + "s")
+	if err != nil && !allowFail {
+		fmt.Println("executeCommand(): command failed: "+command, err)
+		fmt.Println("executeCommand(): Output: " + string(out))
+		os.Exit(1)
+	}
+	return er
+}
